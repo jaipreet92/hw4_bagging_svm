6391d5b4a8fe1bd8dad479ff70a996bcc3b67abb
Jaipreet Singh <jaipreet@amazon.com>
2017-11-22 01:05:09 -0800

    Add sampling in Dtree

--- src/dtree.py
+++ src/dtree.py
@@ -1,5 +1,5 @@
 from sklearn.tree import DecisionTreeClassifier
-
+import numpy as np
 
 class Learner(object):
     def train(self, training_data):
@@ -10,14 +10,20 @@ class Learner(object):
 
 
 class DTree(Learner):
-    def __init__(self, max_depth=None):
+    def __init__(self, max_depth=None, sample=False):
         super().__init__()
         self._tree = None
         self._max_depth = max_depth
+        self._sample = sample
 
     def train(self, training_data):
+        training_data_sample  = training_data
+        if self._sample:
+            idxs = np.random.choice(training_data.shape[0], training_data.shape[0], replace=True)
+            training_data_sample = training_data[idxs, :]
+
         tree = DecisionTreeClassifier(criterion='entropy', max_depth=self._max_depth)
-        self._tree = tree.fit(training_data[:, 0:7], training_data[:, 8])
+        self._tree = tree.fit(training_data_sample[:, 0:7], training_data_sample[:, 8])
 
     def test(self, testing_data):
         predicted_vals = self._tree.predict(testing_data[:, 0:7])
